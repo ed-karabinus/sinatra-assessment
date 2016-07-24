ec3801b2006627ac5c3fc1b3ad2f227ed4f59103
ed-karabinus <ejkarabin@gmail.com>
2016-07-24 06:25:55 -0500

    I fixed a typo in app/models/category.rb.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
   belongs_to :user
   has_many :components
   validates_presence_of :name, :description
-  validates_presence of :user_id, on: :create
+  validates_presence_of :user_id, on: :create
 end
